9ebca276d792970f00234c3e77d893194789eb84
Jeff Lai <jeffrey.lai@salesforce.com>
2012-08-15 16:48:32 -0700

    Revert "Refactor common code to abstrac superclass"
    This reverts commit aea334f8c3046d1ffe9fe0bf287d4c0df0a1e690.

--- src/main/java/com/sforce/ws/transport/JdkHttpTransport.java
+++ src/main/java/com/sforce/ws/transport/JdkHttpTransport.java
@@ -25,16 +25,18 @@
  */
 package com.sforce.ws.transport;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
-import com.sforce.ws.ConnectorConfig;
+import com.sforce.ws.*;
 import com.sforce.ws.tools.VersionInfo;
 import com.sforce.ws.util.Base64;
+import com.sforce.ws.util.FileUtil;
 
 /**
  * This class is an implementation of Transport using the build in
@@ -44,14 +46,94 @@ import com.sforce.ws.util.Base64;
  * @version 1.0
  * @since 1.0  Nov 30, 2005
  */
-public class JdkHttpTransport extends AbstractTransport  {
+public class JdkHttpTransport implements Transport {
+    private HttpURLConnection connection;
+    private boolean successful;
+    private ConnectorConfig config;
+	private URL url;
+
     public JdkHttpTransport() {
-    	super();
     }
 		
     public JdkHttpTransport(ConnectorConfig config) {
-        super(config);
-    } 
+        setConfig(config);
+    }
+
+    @Override
+    public void setConfig(ConnectorConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public OutputStream connect(String uri, HashMap<String, String> httpHeaders) throws IOException {
+        return connectLocal(uri, httpHeaders, true);
+    }
+
+    @Override
+    public OutputStream connect(String uri, HashMap<String, String> httpHeaders, boolean enableCompression)
+            throws IOException {
+        return connectLocal(uri, httpHeaders, enableCompression);
+    }
+
+    @Override
+    public OutputStream connect(String uri, String soapAction) throws IOException {
+        if (soapAction == null) {
+            soapAction = "";
+        }
+
+        HashMap<String, String> header = new HashMap<String, String>();
+
+        header.put("SOAPAction", "\"" + soapAction + "\"");
+        header.put("Content-Type", "text/xml; charset=UTF-8");
+        header.put("Accept", "text/xml");
+
+        return connectLocal(uri, header);
+    }
+
+    private OutputStream connectLocal(String uri, HashMap<String, String> httpHeaders) throws IOException {
+        return connectLocal(uri, httpHeaders, true);
+    }
+
+    private OutputStream connectLocal(String uri, HashMap<String, String> httpHeaders, boolean enableCompression)
+            throws IOException {
+        return wrapOutput(connectRaw(uri, httpHeaders, enableCompression), enableCompression);
+    }
+
+    private OutputStream wrapOutput(OutputStream output, boolean enableCompression) throws IOException {
+        if (config.getMaxRequestSize() > 0) {
+            output = new LimitingOutputStream(config.getMaxRequestSize(), output);
+        }
+
+        // when we are writing a zip file we don't bother with compression
+        if (enableCompression && config.isCompression()) {
+            output = new GZIPOutputStream(output);
+        }
+
+        if (config.isTraceMessage()) {
+            output = config.teeOutputStream(output);
+        }
+
+        if (config.hasMessageHandlers()) {
+            output = new MessageHandlerOutputStream(config, url, output);
+        }
+
+        return output;
+    }
+
+    private OutputStream connectRaw(String uri, HashMap<String, String> httpHeaders, boolean enableCompression)
+            throws IOException {
+        url = new URL(uri);
+
+        connection = createConnection(config, url, httpHeaders, enableCompression);
+        connection.setRequestMethod("POST");
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        if (config.useChunkedPost()) {
+            connection.setChunkedStreamingMode(4096);
+        }
+
+        return connection.getOutputStream();
+    }
 
     public static HttpURLConnection createConnection(ConnectorConfig config, URL url,
             HashMap<String, String> httpHeaders) throws IOException {
@@ -107,11 +189,65 @@ public class JdkHttpTransport extends AbstractTransport  {
         return connection;
     }
 
-	@Override
-	protected HttpURLConnection createHttpURLConnection(
-			ConnectorConfig config2, URL url2,
-			HashMap<String, String> httpHeaders, boolean enableCompression) throws IOException {
-		return createConnection(config, url, httpHeaders, enableCompression);
-	}
+    @Override
+    public InputStream getContent() throws IOException {
+        InputStream in;
+
+        try {
+            successful = true;
+            in = connection.getInputStream();
+        } catch (IOException e) {
+            successful = false;
+            in = connection.getErrorStream();
+            if (in == null) {
+                throw e;
+            }
+        }
+
+        String encoding = connection.getHeaderField("Content-Encoding");
+
+        if (config.getMaxResponseSize() > 0) {
+            in = new LimitingInputStream(config.getMaxResponseSize(), in);
+        }
+
+        if ("gzip".equals(encoding)) {
+            in = new GZIPInputStream(in);
+        }
+
+        if (config.hasMessageHandlers() || config.isTraceMessage()) {
+            byte[] bytes = FileUtil.toBytes(in);
+            in = new ByteArrayInputStream(bytes);
+
+            if (config.hasMessageHandlers()) {
+                Iterator<MessageHandler> it = config.getMessagerHandlers();
+                while(it.hasNext()) {
+                    MessageHandler handler = it.next();
+                    if (handler instanceof MessageHandlerWithHeaders) {
+                        ((MessageHandlerWithHeaders) handler).handleResponse(url, bytes, connection.getHeaderFields());
+                    } else {
+                        handler.handleResponse(url, bytes);
+                    }
+                }
+            }
+
+            if (config.isTraceMessage()) {
+                Map<String, List<String>> headers = connection.getHeaderFields();
+                for (Map.Entry header : headers.entrySet()) {
+                    config.getTraceStream().print(header.getKey());
+                    config.getTraceStream().print("=");
+                    config.getTraceStream().println(header.getValue());
+                }
+                
+                config.teeInputStream(bytes);
+            }
+        }
+
+        return in;
+    }
+
+    @Override
+    public boolean isSuccessful() {
+        return successful;
+    }
 
 }
