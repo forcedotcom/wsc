bbf4d1a4beb8774a0fed60618d42563019a3bf08
Jeff Lai <jeffrey.lai@salesforce.com>
2013-03-12 17:23:17 -0700

    fix tests that were failing

--- src/test/java/com/sforce/ws/wsdl/WsdlFactoryTest.java
+++ src/test/java/com/sforce/ws/wsdl/WsdlFactoryTest.java
@@ -5,9 +5,8 @@
  */
 package com.sforce.ws.wsdl;
 
-import java.net.URL;
-
 import junit.framework.TestCase;
+
 import org.junit.Assert;
 
 /**
@@ -19,26 +18,13 @@ import org.junit.Assert;
 public class WsdlFactoryTest extends TestCase {
 
     public void testWsdlElement() throws Exception {
-        Definitions definitions = WsdlFactory.create(new URL(getSourcePath() + "src/com/sforce/ws/wsdl/WsdlRefUnitTest.wsdl"));
+        Definitions definitions = WsdlFactory.create(getClass().getClassLoader().getResource("WsdlRefUnitTest.wsdl"));
         Assert.assertNotNull(definitions);
     }
 
     public void testAttribute() throws Exception {
-        Definitions definitions = WsdlFactory.create(new URL(getSourcePath() + "src/com/sforce/ws/wsdl/AttributeUnitTest.wsdl"));
+        Definitions definitions = WsdlFactory.create(getClass().getClassLoader().getResource("AttributeUnitTest.wsdl"));
         Assert.assertNotNull(definitions);
     }
 
-    private String getSourcePath() {
-        ClassLoader loader = getClass().getClassLoader();
-        String url = loader.getResource(WsdlFactoryTest.class.getName().replace(".", "/") + ".class").toString();
-        int index = url.lastIndexOf("classes");
-
-        if (index == -1) {
-            throw new RuntimeException("Failed to find source path");
-        } else {
-            url = url.substring(0, index);
-        }
-
-        return url;
-    }
 }
--- src/test/java/com/sforce/ws/wsdl/WsdlRefUnitTest.java
+++ src/test/java/com/sforce/ws/wsdl/WsdlRefUnitTest.java
@@ -5,7 +5,6 @@ package com.sforce.ws.wsdl;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 import junit.framework.TestCase;
 
@@ -20,24 +19,11 @@ import com.sforce.ws.parser.PullParserException;
 public class WsdlRefUnitTest extends TestCase {
 
     public void testWsdlElement() throws PullParserException, WsdlParseException, MalformedURLException, IOException {
-        WsdlFactory.create(new URL(getSourcePath() + "src/com/sforce/ws/wsdl/WsdlRefUnitTest.wsdl"));
+        WsdlFactory.create(getClass().getClassLoader().getResource("WsdlRefUnitTest.wsdl"));
     }
 
     public void testAttribute() throws WsdlParseException, MalformedURLException, IOException {
-        WsdlFactory.create(new URL(getSourcePath() + "src/com/sforce/ws/wsdl/AttributeUnitTest.wsdl"));
+        WsdlFactory.create(getClass().getClassLoader().getResource("AttributeUnitTest.wsdl"));
     }
 
-    private String getSourcePath() {
-        ClassLoader loader = getClass().getClassLoader();
-        String url = loader.getResource(WsdlRefUnitTest.class.getName().replace(".", "/") + ".class").toString();
-        int index = url.lastIndexOf("classes");
-
-        if (index == -1) {
-            throw new RuntimeException("Failed to find source path");
-        } else {
-            url = url.substring(0, index);
-        }
-
-        return url;
-    }
 }
