e89eab2c98248e2593f46327dd24c88b48df1f4d
ZiglioNZ <zigliolie@sirtrack.com>
2013-05-08 11:59:03 +1200

    Strip text from the beginning of the response

--- src/main/java/com/sforce/ws/transport/GaeHttpTransport.java
+++ src/main/java/com/sforce/ws/transport/GaeHttpTransport.java
@@ -28,128 +28,249 @@ package com.sforce.ws.transport;
 import java.io.*;
 import java.net.URL;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.sforce.ws.ConnectorConfig;
+import com.sforce.ws.MessageHandler;
 import com.sforce.ws.tools.VersionInfo;
+import com.sforce.ws.util.Base64;
 
 import com.google.appengine.api.urlfetch.*;
 
 /**
- * This class is an implementation of Transport using the
- * Google AppEngine urlFetch
- *
+ * This class is an implementation of Transport using the Google AppEngine
+ * urlFetch
+ * 
  * @author Ron Hess
  * @version 1.0
- * @since 1.0  Jan 29 2009
+ * @since 1.0 Jan 29 2009
  */
 public class GaeHttpTransport implements Transport {
 
-    private static Logger log = Logger.getLogger("soapconnection send");
-    private URLFetchService connection;
-    private boolean successful;
-   
-    private ConnectorConfig config; // not much is used from here
-    private URL url;
-    private HTTPRequest request;
-       
-        /* soapConnection writes into this stream where it is collected
-         * until getContent() is called, then we take the bytes and put
-         * them into the request using setPayload()
-         */
-    private ByteArrayOutputStream output;
-       
-    public GaeHttpTransport() {
-        this.connection = URLFetchServiceFactory.getURLFetchService();
+  private static Logger log = Logger.getLogger("soapconnection send");
+  private URLFetchService connection;
+  private HTTPRequest request;
+
+  private boolean successful;
+
+  private ConnectorConfig config; // not much is used from here
+  private URL url;
+
+  private ByteArrayOutputStream output;
+
+  public GaeHttpTransport() {
+    this.connection = URLFetchServiceFactory.getURLFetchService();
+  }
+
+  public void setConfig(ConnectorConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public OutputStream connect(String uri, String soapAction) throws IOException {
+    if (soapAction == null) {
+      soapAction = "";
     }
 
-    public void setConfig(ConnectorConfig config) {
-        this.config = config;
+    HashMap<String, String> headers = new HashMap<String, String>();
+
+    headers.put("Content-Type", "text/xml; charset=UTF-8");
+    headers.put("Accept", "text/xml");
+    headers.put("User-Agent", VersionInfo.info());
+    headers.put("SOAPAction", "\"" + soapAction + "\"");
+
+    return connect(uri, headers, true);
+  }
+
+  @Override
+  public OutputStream connect(String endpoint, HashMap<String, String> headers) throws IOException {
+    return connect(endpoint, headers, true);
+  }
+
+  @Override
+  public OutputStream connect(String uri, HashMap<String, String> httpHeaders, boolean enableCompression) throws IOException {
+    return wrapOutput(connectRaw(uri, httpHeaders, enableCompression),enableCompression);
+  }
+
+  private OutputStream wrapOutput(OutputStream output, boolean enableCompression) throws IOException {
+    if (config.getMaxRequestSize() > 0) {
+      output = new LimitingOutputStream(config.getMaxRequestSize(), output);
     }
 
-    @Override
-    public OutputStream connect(String uri, String soapAction) throws IOException {
-        if (soapAction == null) {
-            soapAction = "";
-        }
-
-        HashMap<String, String> headers = new HashMap<String, String>();
-
-        headers.put("Content-Type", "text/xml; charset=UTF-8");
-        headers.put("Accept", "text/xml");
-        headers.put("User-Agent", VersionInfo.info());
-        headers.put("SOAPAction", "\"" + soapAction + "\"");
-        
-        return connect(uri, headers, true);
+    // when we are writing a zip file we don't bother with compression
+    if (enableCompression && config.isCompression()) {
+      output = new GZIPOutputStream(output);
     }
 
+//    if (config.isTraceMessage()) {
+//      output = new TeeOutputStream(output, config.getTraceStream());
+//    }
+//
+//    if (config.hasMessageHandlers()) {
+//      output = new MessageHandlerOutputStream(url, output,
+//          config.getMessagerHandlers());
+//    }
 
-    @Override
-    public OutputStream connect(String endpoint, HashMap<String, String> headers) throws IOException {
-        return connect(endpoint, headers, true);
+    return output;
+  }
+
+  private OutputStream connectRaw(String uri, Map<String, String> httpHeaders,
+      boolean enableCompression) throws IOException {
+
+    log.info(uri);
+
+    url = new URL(uri);
+
+    request = createRequest(config, url, httpHeaders, enableCompression);
+
+    output = new ByteArrayOutputStream();
+    return output;
+
+  }
+
+  public static HTTPRequest createRequest(ConnectorConfig config, URL url,
+      Map<String, String> httpHeaders) throws IOException {
+    return createRequest(config, url, httpHeaders, true);
+  }
+
+  public static HTTPRequest createRequest(ConnectorConfig config, URL url,
+      Map<String, String> httpHeaders, boolean enableCompression)
+      throws IOException {
+
+    if (config.isTraceMessage()) {
+      config.getTraceStream().println(
+          "WSC: Creating a new connection to " + url + " Proxy = "
+              + config.getProxy() + " username " + config.getProxyUsername());
     }
 
-	@Override
-	public OutputStream connect(String endpoint, HashMap<String, String> httpHeaders, boolean enableCompression) throws IOException {
-        url = new URL(endpoint);
-        
-        if (config.getReadTimeout() > 0) {
-        	FetchOptions fetchOptions = FetchOptions.Builder.withDeadline(config.getReadTimeout()/1000);
-            request = new HTTPRequest(url, HTTPMethod.POST, fetchOptions);
-        } else {
-            request = new HTTPRequest(url, HTTPMethod.POST);
-        }
-        
-        for (Map.Entry<String, String> hdr : httpHeaders.entrySet()) {
-            request.addHeader( new HTTPHeader(hdr.getKey(), hdr.getValue()) );
-        }
+    FetchOptions fetchOptions = FetchOptions.Builder.withDeadline(config
+        .getReadTimeout() / 1000);
+    HTTPRequest request = new HTTPRequest(url, HTTPMethod.POST, fetchOptions);
 
-        output = new ByteArrayOutputStream();
-        return output;
+    if (httpHeaders == null
+        || (httpHeaders.get("User-Agent") == null && httpHeaders
+            .get("user-agent") == null)) {
+      request.addHeader(new HTTPHeader("User-Agent", VersionInfo.info()));
     }
 
-    public InputStream getContent() throws IOException {
- 
-        InputStream in;        
-        try {
-            successful = true;
-            if (config.isTraceMessage()) {
-                log.info( output.toString() );   // educational
-            }
-           
-            // payload comes from bytes written to the output stream
-            this.request.setPayload( output.toByteArray() );
-           
-            // go get that soap, read/write to Force.com API
-            HTTPResponse resp = connection.fetch( this.request );
-            if (resp.getResponseCode() != 200) {
-                successful = false;
-            }
-           
-            byte[] bytes = resp.getContent();
-           
-            // dump out the response
-            if (config.isTraceMessage()) {
-                log.info( new String(bytes) );
-            }
-            in = new ByteArrayInputStream(bytes);
-           
-        } catch (IOException e) {
-            successful = false;
-            in = new ByteArrayInputStream( e.getMessage().getBytes() );
-            log.warning("getContent: "+e.getMessage());
-        }
-
-        return in;
+    if (config.getHeaders() != null) {
+      for (Entry<String, String> ent : config.getHeaders().entrySet()) {
+        request.addHeader(new HTTPHeader(ent.getKey(), ent.getValue()));
+      }
     }
 
-    public boolean isSuccessful() {   
-        return successful;    
+    if (enableCompression && config.isCompression()) {
+      request.addHeader(new HTTPHeader("Content-Encoding", "gzip"));
+      request.addHeader(new HTTPHeader("Accept-Encoding", "gzip"));
     }
+
+    if (config.getProxyUsername() != null) {
+      String token = config.getProxyUsername() + ":"
+          + config.getProxyPassword();
+      String auth = "Basic " + new String(Base64.encode(token.getBytes()));
+      request.addHeader(new HTTPHeader("Proxy-Authorization", auth));
+      request.addHeader(new HTTPHeader("Https-Proxy-Authorization", auth));
+    }
+
+    if (httpHeaders != null) {
+      for (Map.Entry<String, String> entry : httpHeaders.entrySet()) {
+        request.addHeader(new HTTPHeader(entry.getKey(), entry.getValue()));
+      }
+    }
+
+    return request;
+  }
+
+  /** Gets the values of all headers with the name {@code headerName}. */
+  List<String> getHeaders(HTTPResponse resp, String headerName) {
+    List<String> b = new ArrayList<String>();
+    for (HTTPHeader h : resp.getHeadersUncombined()) {
+      if (headerName.equalsIgnoreCase(h.getName())) {
+        b.add(h.getValue());
+      }
+    }
+    return b;
+  }
+
+  String getHeader(HTTPResponse resp, String headerName) {
+    for (HTTPHeader h : resp.getHeadersUncombined()) {
+      if (headerName.equalsIgnoreCase(h.getName())) {
+        return h.getValue();
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public InputStream getContent() throws IOException {
+    InputStream in;
+    HTTPResponse resp;
+
+    try {
+      successful = true;
+      if (config.isTraceMessage()) {
+        log.info(output.toString()); // educational
+      }
+
+      // payload comes from bytes written to the output stream
+      this.request.setPayload(output.toByteArray());
+
+      // go get that soap, read/write to Force.com API
+      resp = connection.fetch(this.request);
+
+      byte[] bytes = resp.getContent();
+
+      final String contentid = "Content-ID: <root.message@cxf.apache.org>";
+      String str = new String(bytes);
+      if (str.contains(contentid)) {
+        str = str.split(contentid)[1].trim();
+      }
+      bytes = str.getBytes();
+
+      // dump out the response
+      if (config.isTraceMessage()) {
+        log.info(new String(bytes));
+      }
+      in = new ByteArrayInputStream(bytes);
+
+      this.successful = resp.getResponseCode() < 400;
+      String encoding = getHeader(resp, "Content-Encoding");
+
+      if (config.getMaxResponseSize() > 0) {
+        in = new LimitingInputStream(config.getMaxResponseSize(), in);
+      }
+
+      if ("gzip".equals(encoding)) {
+        in = new GZIPInputStream(in);
+      }
+
+      // dump out the response
+      if (config.isTraceMessage()) {
+        log.info(new String(bytes));
+      }
+    } catch (IOException e) {
+      successful = false;
+      in = new ByteArrayInputStream(e.getMessage().getBytes());
+      log.warning("getContent: " + e.getMessage());
+    }
+    return in;
+
+  }
+
+  public boolean isSuccessful() {
+    return successful;
+  }
+
 }
