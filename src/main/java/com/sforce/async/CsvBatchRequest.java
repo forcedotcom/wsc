e01975e47a4654acad23bbff8ad38da2a0ab542a
donr7n <drobertson@salesforce.com>
2013-10-29 14:55:38 -0700

    Added foreign CSV+transformation feature to BulkConnection and also added a new batch request class for CSV content.

---
+++ src/main/java/com/sforce/async/CsvBatchRequest.java
@@ -0,0 +1,93 @@
+package com.sforce.async;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.parser.PullParserException;
+import com.sforce.ws.transport.Transport;
+
+/**
+ * CSV batch request.
+ * 
+ * @author drobertson
+ * @since 188
+ *
+ */
+public class CsvBatchRequest {
+
+    private final OutputStream csvStream;
+    private final Transport transport;
+    private boolean isNew;
+    
+    private final String UTF8 = "UTF-8";
+
+    public CsvBatchRequest(Transport transport, OutputStream out) throws IOException {
+        this.transport = transport;
+        this.csvStream = out;
+        
+        this.isNew = true;
+    }
+
+    public void addHeader(String[] columns) throws AsyncApiException {
+    	if (isNew) {
+    		addRow(columns);
+    	}
+    	else {
+            throw new AsyncApiException("Can't add header to populated CSV", AsyncExceptionCode.ClientInputError);
+    	}
+    }
+    
+    
+    public void addRow(String[] columns) throws AsyncApiException {
+        try {
+        	for (int i = 0; i < columns.length; ++i) {
+                if (i == 0) {
+                	addFirstColumn(columns[i]);
+                }
+                else {
+                	addColumn(columns[i]);
+                }
+        	}
+        	isNew = false;
+        } catch (IOException e) {
+            throw new AsyncApiException("Failed to add row", AsyncExceptionCode.ClientInputError, e);
+        }
+    }
+
+    public BatchInfo completeRequest() throws AsyncApiException {
+        try {
+            csvStream.close();
+            InputStream in = transport.getContent();
+
+            if (transport.isSuccessful()) {
+                return BatchRequest.loadBatchInfo(in);
+            } else {
+                BulkConnection.parseAndThrowException(in);
+            }
+        } catch(IOException e) {
+            throw new AsyncApiException("Failed to complete request", AsyncExceptionCode.ClientInputError, e);
+        } catch (PullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return null;
+    }
+    
+    private void addFirstColumn(String value) throws IOException {
+    	csvStream.write(",".getBytes(UTF8));
+    	addColumn(value);
+    }
+    
+    private void addColumn(String value) throws IOException {
+    	if (value != null) {
+        	csvStream.write("\"".getBytes(UTF8));
+        	csvStream.write(value.replace("\"", "\"\"").getBytes(UTF8));
+        	csvStream.write("\"".getBytes(UTF8));
+    	}
+    }
+} 
